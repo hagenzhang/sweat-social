b2c5935424e25e7d34d5cdc96a8a57dbf8405845
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-15 01:56:02 -0500

    creating empty swift project

---
+++ sweat-social/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sweat-social
+//
+//  Created by Hagen Zhang on 11/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
