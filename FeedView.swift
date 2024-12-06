274eccab55c3d2f1115f11630e58e584da740dbd
Christine Lee <118480924+stinejlee@users.noreply.github.com>
2024-12-06 17:02:09 -0500

    Adding tableview to feed
    Hopefilly this works??

---
+++ FeedView.swift
@@ -0,0 +1,52 @@
+//
+//  FeedView.swift
+//  sweat-social
+//
+
+import UIKit
+
+class FeedView: UIView {
+    var toolbar:UIToolbar!
+    
+    var tableViewPosts: UITableView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setupToolbar()
+        setupTableViewPosts()
+        initConstraints()
+    }
+    
+    func setupToolbar(){
+        toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.safeAreaLayoutGuide.widthAnchor.hashValue, height: 50))
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(toolbar)
+    }
+    
+    func setupTableViewPosts(){
+        tableViewPosts = UITableView()
+        tableViewPosts.register(PostTableViewCell.self, forCellReuseIdentifier: "posts")
+        tableViewPosts.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(tableViewPosts)
+    }
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            toolbar.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            toolbar.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            toolbar.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            
+            tableViewPosts.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 32),
+            tableViewPosts.bottomAnchor.constraint(equalTo: toolbar.topAnchor),
+            tableViewPosts.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            tableViewPosts.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
---
+++ PostTableViewCell.swift
@@ -0,0 +1,139 @@
+//
+//  PostTableViewCell.swift
+//  sweat-social
+//
+
+import UIKit
+import SwiftUI
+
+class PostTableViewCell: UITableViewCell {
+    
+    var wrapperCellView: UIView!
+    
+    // above the image,
+    var profileImage: UIImageView!
+    var nameLabel: UILabel!
+    
+    // handles the caption section
+    var likedSymbol: UIImageView!
+    var caption: UILabel!
+    
+<<<<<<< HEAD
+    // temp objects TODO
+    var tempRect: UIImageView!
+//    var exerciseTable: PostExerciseTable!
+    
+    
+=======
+>>>>>>> 6f0712ae4f6d8c451c437b1a56693007e8875599
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupWrapperCellView()
+        setupProfileImage()
+        setupNameLabel()
+        
+        setupLikeSymbol()
+        setupCaption()
+        
+        initConstraints()
+    }
+    
+    func setupWrapperCellView(){
+        wrapperCellView = UITableViewCell()
+        wrapperCellView.backgroundColor = .white
+        wrapperCellView.layer.cornerRadius = 6.0
+        wrapperCellView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(wrapperCellView)
+    }
+    
+    func setupProfileImage() {
+        profileImage = UIImageView()
+        profileImage.image = UIImage(systemName: "person.circle")
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.addSubview(profileImage)
+    }
+    
+    func setupNameLabel() {
+        nameLabel = UILabel()
+        nameLabel.text = "placeholder name"
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.addSubview(nameLabel)
+    }
+    
+    // func setupPostImage() { }
+
+    func setupLikeSymbol() {
+        likedSymbol = UIImageView(image: UIImage(systemName: "heart.fill"))
+        likedSymbol.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.addSubview(likedSymbol)
+    }
+    
+    func setupCaption() {
+        caption = UILabel()
+        
+//        caption.text = "This is going to represent a really long caption! I'm sort of curious how it well " +
+//            "render and fare, plus some people loooove to write a whole bunch down here so it might be nice " +
+//            "to see if it can handle such a load. Obviously this is a placeholder."
+//
+        caption.text = "Caption"
+        caption.textColor = .black
+        caption.font = caption.font?.withSize(12)
+        caption.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.addSubview(caption)
+    }
+    
+    
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            wrapperCellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            wrapperCellView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            wrapperCellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            wrapperCellView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            
+            profileImage.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 5),
+            profileImage.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 5),
+            profileImage.heightAnchor.constraint(equalToConstant: 20),
+            
+            nameLabel.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 5),
+            nameLabel.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 5),
+            
+<<<<<<< HEAD
+            // stand-in for where the post photo will go
+            tempRect.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 5),
+            tempRect.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor),
+            tempRect.trailingAnchor.constraint(equalTo: wrapperCellView.trailingAnchor),
+            tempRect.heightAnchor.constraint(equalToConstant: 100),
+            
+            likedSymbol.topAnchor.constraint(equalTo: tempRect.bottomAnchor, constant: 5),
+=======
+            likedSymbol.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+>>>>>>> 6f0712ae4f6d8c451c437b1a56693007e8875599
+            likedSymbol.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 5),
+            likedSymbol.heightAnchor.constraint(equalToConstant: 20),
+            
+            caption.topAnchor.constraint(equalTo: likedSymbol.bottomAnchor, constant: 5),
+            caption.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 5),
+            caption.trailingAnchor.constraint(equalTo: wrapperCellView.trailingAnchor, constant: -5),
+            
+            wrapperCellView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
